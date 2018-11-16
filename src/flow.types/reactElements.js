a7ea09e37870075ebb5e920c6f443178db854803
svolynets <svolynets@s-pro.io>
2018-11-16 16:32:20 +0200

    Added types

---
+++ src/flow.types/reactElements.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Volynets Serhii
+ * @Date: 2018-11-12 13:26:07
+ * @Last Modified by: Volynets Serhii
+ * @Last Modified time: 2018-11-12 18:07:34
+ * @flow
+ */
+export type _t_styles = Object | number | Array<Object> | Array<number>;
+
+export type _t_icon_types = "ionicons" | "simple" | "material";
