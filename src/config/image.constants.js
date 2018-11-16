fa0330090416317c499efb5d2ecef3968040b0b2
svolynets <svolynets@s-pro.io>
2018-11-16 16:30:36 +0200

    Added config

---
+++ src/config/image.constants.js
@@ -0,0 +1,60 @@
+/*
+ * @Author: Volynets Serhii
+ * @Date: 2018-11-12 12:50:37
+ * @Last Modified by: Volynets Serhii
+ * @Last Modified time: 2018-11-16 13:57:48
+ * @flow
+ */
+import BACK_ARROW from 'src/assets/images/backArrow.png';
+import HOMMEPAGE_AVATAR from 'src/assets/images/homepageAvatar.png';
+import HOMMEPAGE_BANNER from 'src/assets/images/homepageBanner.png';
+import HOMMEPAGE_FILTER from 'src/assets/images/homepageFilter.png';
+import HOMMEPAGE_STAR from 'src/assets/images/homepageStar.png';
+import LOGIN_BG from 'src/assets/images/loginBG.png';
+import LOGOS_BK from 'src/assets/images/logosBK.png';
+import LOGOS_HM from 'src/assets/images/logosHM.png';
+import LOGOS_MAC from 'src/assets/images/logosMac.png';
+import OFFER_FAVORITES from 'src/assets/images/offerFavorites.png';
+import ONBOARDING_BG from 'src/assets/images/onboardingBG.png';
+import ONBOARDING_FACEBOOK from 'src/assets/images/onboardingFacebook.png';
+import ONBOARDING_GOOGLE from 'src/assets/images/onboardingGoogle.png';
+import SPLASH_BG from 'src/assets/images/splashBG.png';
+import SPLASH_LOGO from 'src/assets/images/splashLogo.png';
+import TABS_FAVORITES from 'src/assets/images/tabsFavorites.png';
+import TABS_HOME from 'src/assets/images/tabsHome.png';
+import TABS_SEARCH from 'src/assets/images/tabsSearch.png';
+import TABS_SETTINGS from 'src/assets/images/tabsSettings.png';
+import WELCOME_BG from 'src/assets/images/welcomeBG.png';
+import OFFER_CARUSEL from 'src/assets/images/offerCarusel.png';
+import OFFER_ICON_LOCATION from 'src/assets/images/offerIconLocation.png';
+import OFFER_ICON_PHONE from 'src/assets/images/offerIconPhone.png';
+import OFFER_ICON_WEB from 'src/assets/images/offerIconWeb.png';
+
+const IMAGES = {
+  BACK_ARROW,
+  HOMMEPAGE_AVATAR,
+  HOMMEPAGE_BANNER,
+  HOMMEPAGE_FILTER,
+  HOMMEPAGE_STAR,
+  LOGIN_BG,
+  LOGOS_BK,
+  LOGOS_HM,
+  LOGOS_MAC,
+  OFFER_FAVORITES,
+  ONBOARDING_BG,
+  ONBOARDING_FACEBOOK,
+  ONBOARDING_GOOGLE,
+  SPLASH_BG,
+  SPLASH_LOGO,
+  TABS_FAVORITES,
+  TABS_HOME,
+  TABS_SEARCH,
+  TABS_SETTINGS,
+  WELCOME_BG,
+  OFFER_CARUSEL,
+  OFFER_ICON_LOCATION,
+  OFFER_ICON_PHONE,
+  OFFER_ICON_WEB,
+};
+
+export default IMAGES;
